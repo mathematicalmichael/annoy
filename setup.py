541adcda4a5d58ad03a36475131244753aa8831c
Michael Pilosov <consistentbayes@gmail.com>
2025-12-02 20:00:35 +0000

    1.17.3 release

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ if manual_linker_args:
     extra_link_args = manual_linker_args.split(',')
 
 setup(name='annoy-mm',
-      version='1.17.4-rc5',
+      version='1.17.3',
       description='Approximate Nearest Neighbors in C++/Python optimized for memory usage and loading/saving to disk.',
       packages=['annoy'],
       package_data={'annoy': ['__init__.pyi', 'py.typed']},
